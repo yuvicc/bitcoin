d7230ea9b9ee866909ed52033be4d62815b303ee
TheCharlatan <seb.kung@gmail.com>
2024-05-29 16:13:45 +0200

    kernel: Add logging to kernel library C header
    Exposing logging in the kernel library allows users to follow what is
    going on when using it. Users of the C header can use
    `kernel_logging_connection_create(...)` to pass a callback function to
    Bitcoin Core's internal logger. Additionally the level and severity can
    be globally configured.
    
    By default, the logger buffers messages until
    `kernel_loggin_connection_create(...)` is called. If the user does not
    want any logging messages, it is recommended that
    `kernel_disable_logging()` is called, which permanently disables the
    logging and any buffering of messages.

--- src/kernel/bitcoinkernel.cpp
+++ src/kernel/bitcoinkernel.cpp
@@ -8,17 +8,21 @@
 
 #include <consensus/amount.h>
 #include <kernel/context.h>
+#include <logging.h>
 #include <primitives/transaction.h>
 #include <script/interpreter.h>
 #include <script/script.h>
 #include <serialize.h>
 #include <streams.h>
+#include <tinyformat.h>
 #include <util/translation.h>
 
+#include <cassert>
 #include <cstddef>
 #include <cstring>
 #include <exception>
 #include <functional>
+#include <list>
 #include <span>
 #include <string>
 #include <utility>
@@ -43,6 +47,62 @@ bool is_valid_flag_combination(unsigned int flags)
     if (flags & SCRIPT_VERIFY_CLEANSTACK && ~flags & (SCRIPT_VERIFY_P2SH | SCRIPT_VERIFY_WITNESS)) return false;
     if (flags & SCRIPT_VERIFY_WITNESS && ~flags & SCRIPT_VERIFY_P2SH) return false;
     return true;
+}
+
+BCLog::Level get_bclog_level(const kernel_LogLevel level)
+{
+    switch (level) {
+    case kernel_LogLevel::kernel_LOG_INFO: {
+        return BCLog::Level::Info;
+    }
+    case kernel_LogLevel::kernel_LOG_DEBUG: {
+        return BCLog::Level::Debug;
+    }
+    case kernel_LogLevel::kernel_LOG_TRACE: {
+        return BCLog::Level::Trace;
+    }
+    } // no default case, so the compiler can warn about missing cases
+    assert(false);
+}
+
+BCLog::LogFlags get_bclog_flag(const kernel_LogCategory category)
+{
+    switch (category) {
+    case kernel_LogCategory::kernel_LOG_BENCH: {
+        return BCLog::LogFlags::BENCH;
+    }
+    case kernel_LogCategory::kernel_LOG_BLOCKSTORAGE: {
+        return BCLog::LogFlags::BLOCKSTORAGE;
+    }
+    case kernel_LogCategory::kernel_LOG_COINDB: {
+        return BCLog::LogFlags::COINDB;
+    }
+    case kernel_LogCategory::kernel_LOG_LEVELDB: {
+        return BCLog::LogFlags::LEVELDB;
+    }
+    case kernel_LogCategory::kernel_LOG_MEMPOOL: {
+        return BCLog::LogFlags::MEMPOOL;
+    }
+    case kernel_LogCategory::kernel_LOG_PRUNE: {
+        return BCLog::LogFlags::PRUNE;
+    }
+    case kernel_LogCategory::kernel_LOG_RAND: {
+        return BCLog::LogFlags::RAND;
+    }
+    case kernel_LogCategory::kernel_LOG_REINDEX: {
+        return BCLog::LogFlags::REINDEX;
+    }
+    case kernel_LogCategory::kernel_LOG_VALIDATION: {
+        return BCLog::LogFlags::VALIDATION;
+    }
+    case kernel_LogCategory::kernel_LOG_KERNEL: {
+        return BCLog::LogFlags::KERNEL;
+    }
+    case kernel_LogCategory::kernel_LOG_ALL: {
+        return BCLog::LogFlags::ALL;
+    }
+    } // no default case, so the compiler can warn about missing cases
+    assert(false);
 }
 
 const CTransaction* cast_transaction(const kernel_Transaction* transaction)
@@ -166,4 +226,77 @@ bool kernel_verify_script(const kernel_ScriptPubkey* script_pubkey_,
                         flags,
                         TransactionSignatureChecker(&tx, input_index, amount, txdata, MissingDataBehavior::FAIL),
                         nullptr);
+}
+
+void kernel_add_log_level_category(const kernel_LogCategory category, const kernel_LogLevel level)
+{
+    if (category == kernel_LogCategory::kernel_LOG_ALL) {
+        LogInstance().SetLogLevel(get_bclog_level(level));
+    }
+
+    LogInstance().AddCategoryLogLevel(get_bclog_flag(category), get_bclog_level(level));
+}
+
+void kernel_enable_log_category(const kernel_LogCategory category)
+{
+    LogInstance().EnableCategory(get_bclog_flag(category));
+}
+
+void kernel_disable_log_category(const kernel_LogCategory category)
+{
+    LogInstance().DisableCategory(get_bclog_flag(category));
+}
+
+void kernel_disable_logging()
+{
+    LogInstance().DisableLogging();
+}
+
+kernel_LoggingConnection* kernel_logging_connection_create(kernel_LogCallback callback,
+                                                           const void* user_data,
+                                                           const kernel_LoggingOptions options)
+{
+    LogInstance().m_log_timestamps = options.log_timestamps;
+    LogInstance().m_log_time_micros = options.log_time_micros;
+    LogInstance().m_log_threadnames = options.log_threadnames;
+    LogInstance().m_log_sourcelocations = options.log_sourcelocations;
+    LogInstance().m_always_print_category_level = options.always_print_category_levels;
+
+    auto connection{LogInstance().PushBackCallback([callback, user_data](const std::string& str) { callback((void*)user_data, str.c_str(), str.length()); })};
+
+    try {
+        // Only start logging if we just added the connection.
+        if (LogInstance().NumConnections() == 1 && !LogInstance().StartLogging()) {
+            LogError("Logger start failed.");
+            LogInstance().DeleteCallback(connection);
+            return nullptr;
+        }
+    } catch (std::exception&) {
+        LogError("Logger start failed.");
+        LogInstance().DeleteCallback(connection);
+        return nullptr;
+    }
+
+    LogDebug(BCLog::KERNEL, "Logger connected.");
+
+    auto heap_connection{new std::list<std::function<void(const std::string&)>>::iterator(connection)};
+    return reinterpret_cast<kernel_LoggingConnection*>(heap_connection);
+}
+
+void kernel_logging_connection_destroy(kernel_LoggingConnection* connection_)
+{
+    auto connection{reinterpret_cast<std::list<std::function<void(const std::string&)>>::iterator*>(connection_)};
+    if (!connection) {
+        return;
+    }
+
+    LogDebug(BCLog::KERNEL, "Logger disconnected.");
+    LogInstance().DeleteCallback(*connection);
+    delete connection;
+
+    // We are not buffering if we have a connection, so check that it is not the
+    // last available connection.
+    if (!LogInstance().Enabled()) {
+        LogInstance().DisconnectTestLogger();
+    }
 }
