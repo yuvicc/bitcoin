41306f081ad1d95149abb21f067b607bac326572
TheCharlatan <seb.kung@gmail.com>
2024-06-01 11:52:52 +0200

    kernel: Add function for copying  block data to C header
    This adds functions for copying serialized block data into a user-owned
    variable-sized byte array.
    
    Use it in the tests for verifying the implementation of the validation
    interface's `BlockChecked` method.

--- src/kernel/bitcoinkernel.h
+++ src/kernel/bitcoinkernel.h
@@ -389,6 +389,14 @@ typedef enum {
     kernel_CHAIN_TYPE_REGTEST,
 } kernel_ChainType;
 
+/**
+ * Convenience struct for holding serialized data.
+ */
+typedef struct {
+    unsigned char* data;
+    size_t size;
+} kernel_ByteArray;
+
 /** @name Transaction
  * Functions for working with transactions.
  */
@@ -836,9 +844,41 @@ BITCOINKERNEL_API kernel_Block* BITCOINKERNEL_WARN_UNUSED_RESULT kernel_block_cr
 ) BITCOINKERNEL_ARG_NONNULL(1);
 
 /**
+ * @brief Copies block data into the returned byte array.
+ *
+ * @param[in] block  Non-null.
+ * @return           Allocated byte array holding the block data, or null on error.
+ */
+BITCOINKERNEL_API kernel_ByteArray* BITCOINKERNEL_WARN_UNUSED_RESULT kernel_copy_block_data(
+    kernel_Block* block
+) BITCOINKERNEL_ARG_NONNULL(1);
+
+/**
+ * @brief Copies block data into the returned byte array.
+ *
+ * @param[in] block  Non-null.
+ * @return           Allocated byte array holding the block data, or null on error.
+ */
+BITCOINKERNEL_API kernel_ByteArray* BITCOINKERNEL_WARN_UNUSED_RESULT kernel_copy_block_pointer_data(
+    const kernel_BlockPointer* block
+) BITCOINKERNEL_ARG_NONNULL(1);
+
+/**
  * Destroy the block.
  */
 BITCOINKERNEL_API void kernel_block_destroy(kernel_Block* block);
+
+///@}
+
+/** @name ByteArray
+ * Functions for working with byte arrays.
+ */
+///@{
+
+/**
+ * A helper function for destroying an existing byte array.
+ */
+BITCOINKERNEL_API void kernel_byte_array_destroy(kernel_ByteArray* byte_array);
 
 ///@}
 
