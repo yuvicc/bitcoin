ef603878c2de8fc595e7d2c95ba05c9002668fdc
TheCharlatan <seb.kung@gmail.com>
2024-06-17 22:50:47 +0200

    kernel: Add options for reindexing in C header
    Adds options for wiping the chainstate and block tree indexes to the
    chainstate load options. In combination and once the
    `*_import_blocks(...)` function is added in a later commit, this
    triggers a reindex. For now, it just wipes the existing data.

--- src/kernel/bitcoinkernel.cpp
+++ src/kernel/bitcoinkernel.cpp
@@ -705,6 +705,19 @@ void btck_chainstate_manager_options_destroy(btck_ChainstateManagerOptions* opti
     delete options;
 }
 
+bool btck_chainstate_manager_options_set_wipe_dbs(btck_ChainstateManagerOptions* chainman_opts, bool wipe_block_tree_db, bool wipe_chainstate_db)
+{
+    if (wipe_block_tree_db && !wipe_chainstate_db) {
+        LogError("Wiping the block tree db without also wiping the chainstate db is currently unsupported.");
+        return false;
+    }
+    auto& opts{btck_ChainstateManagerOptions::get(chainman_opts)};
+    LOCK(opts.m_mutex);
+    opts.m_blockman_options.block_tree_db_params.wipe_data = wipe_block_tree_db;
+    opts.m_chainstate_load_options.wipe_chainstate_db = wipe_chainstate_db;
+    return true;
+}
+
 btck_ChainstateManager* btck_chainstate_manager_create(
     const btck_ChainstateManagerOptions* chainman_opts)
 {
