cf7b562f2e31438526e9808e9d38e8d6e7c1b0ba
TheCharlatan <seb.kung@gmail.com>
2024-06-22 21:08:10 +0200

    kernel: Add chainstate load options for in-memory dbs in C header
    This allows a user to run the kernel without creating on-disk files for
    the block tree and chainstate indexes. This is potentially useful in
    scenarios where the user needs to do some ephemeral validation
    operations.
    
    One specific use case is when linearizing the blocks on disk. The block
    files store blocks out of order, so a program may utilize the library
    and its header to read the blocks with one chainstate manager, and then
    write them back in order, and without orphans, with another chainstate
    maanger. To save disk resources and if the indexes are not required once
    done, it may be beneficial to keep the indexes in memory for the
    chainstate manager that writes the blocks back again.

--- src/kernel/bitcoinkernel_wrapper.h
+++ src/kernel/bitcoinkernel_wrapper.h
@@ -678,6 +678,16 @@ public:
         return btck_chainstate_manager_options_set_wipe_dbs(get(), wipe_block_tree, wipe_chainstate) == 0;
     }
 
+    void SetBlockTreeDbInMemory(bool block_tree_db_in_memory)
+    {
+        btck_chainstate_manager_options_set_block_tree_db_in_memory(get(), block_tree_db_in_memory);
+    }
+
+    void SetChainstateDbInMemory(bool chainstate_db_in_memory)
+    {
+        btck_chainstate_manager_options_set_chainstate_db_in_memory(get(), chainstate_db_in_memory);
+    }
+
     friend class ChainMan;
 };
 
