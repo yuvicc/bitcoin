0d4274d9bdda844580c333466e55848feb0b73e6
TheCharlatan <seb.kung@gmail.com>
2025-04-18 12:51:31 +0200

    consensus: Use Coin span in GetTransactionSigOpCost
    Move the responsibility of retrieving coins from GetTransactionSigOpCost
    to its caller.
    
    This is a part of a series of commits for removing access to the
    CCoinsViewCache in consensus verification functions. The goal is to
    allow calling verification functions with pre-fetched, or a user-defined
    set of coins.
    
    Define two explicit template specializations for both a span of
    references to coins and a span of coins. This allows using it for both
    Coin entries referenced from the CCoinsViewCache, and from contiguous
    memory, like the vector in CBlockUndo.

--- src/consensus/tx_verify.h
+++ src/consensus/tx_verify.h
@@ -56,12 +56,13 @@ unsigned int GetP2SHSigOpCount(const CTransaction& tx, const std::span<T> coins)
 
 /**
  * Compute total signature operation cost of a transaction.
- * @param[in] tx     Transaction for which we are computing the cost
- * @param[in] inputs Map of previous transactions that have outputs we're spending
+ * @param[in] tx    Transaction for which we are computing the cost
+ * @param[in] coins Sorted span of Coins containing previous transaction outputs we're spending
  * @param[in] flags Script verification flags
  * @return Total signature operation cost of tx
  */
-int64_t GetTransactionSigOpCost(const CTransaction& tx, const CCoinsViewCache& inputs, script_verify_flags flags);
+template <Consensus::CoinRef T>
+int64_t GetTransactionSigOpCost(const CTransaction& tx, const std::span<T> coins, script_verify_flags flags);
 
 /**
  * Check if transaction is final and can be included in a block with the
