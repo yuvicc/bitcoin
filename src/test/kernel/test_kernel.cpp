d5cf4605309acbb366363bdebabc20344a9c1622
yuvicc <yuvichh01@gmail.com>
2026-07-02 12:27:01 +0530

    kernel: remove `INTERNAL_ERROR` from `btck_ValidationMode`

--- src/test/kernel/test_kernel.cpp
+++ src/test/kernel/test_kernel.cpp
@@ -193,10 +193,6 @@ public:
             }
             return;
         }
-        case ValidationMode::INTERNAL_ERROR: {
-            std::cout << "Internal error" << std::endl;
-            return;
-        }
         }
     }
 
